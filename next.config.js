c3551819ddcf0f47b8a7017aec69cd192b0bf269
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-07-08 01:57:14 +0100

    feat: :sparkles: set main page for blog post listing

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: [
+      "icons",
+      "icons/categories",
+      "icons/socials",
+      "components/main",
+      "components/main/error",
+      "components/login",
+      "components/shared",
+      "components/shared/login/button",
+      "components/detail/post",
+      "components/detail/post/buttons",
+      "components/detail/post/comment",
+      "components/protected/editor/upload",
+      "config/main",
+      "config/detail",
+      "config/shared",
+      "config/shared/dashboard",
+      "config/main/pages",
+      "config/protected",
+      "components/main/header/navigations",
+      "components/main/header/navigations/menu",
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
